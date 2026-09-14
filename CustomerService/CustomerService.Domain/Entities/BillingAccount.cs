c9aafe44f97f35686126f8f75f6f751a39579695
mirkankacan <kacanmirkan@gmail.com>
2026-09-14 15:12:14 +0300

    feat(customer-service): add domain entities and enums
    Adds core CustomerService.Domain model: Customer, IndividualCustomer,
    Address, BillingAccount, ContactMedium, City, District entities along
    with their supporting enums.

---
+++ CustomerService/CustomerService.Domain/Entities/BillingAccount.cs
@@ -0,0 +1,36 @@
+﻿using Core.Domain;
+using CustomerService.Domain.Enums;
+
+namespace CustomerService.Domain.Entities
+{
+    public class BillingAccount : BaseEntity<Guid>
+    {
+        public Guid CustomerId { get; set; }
+        public string AddressId { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Description { get; set; }
+        public BillingAccountType Type { get; set; }
+        public BillingAccountStatus Status { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Address Address { get; set; }
+        public BillingAccount()
+        {
+
+        }
+
+        public BillingAccount(Guid ıd, Guid customerId, string addressId, string name, string number, string description, BillingAccountType type, BillingAccountStatus status)
+        {
+            Id = ıd;
+            CustomerId = customerId;
+            AddressId = addressId;
+            Name = name;
+            Number = number;
+            Description = description;
+            Type = type;
+            Status = status;
+
+        }
+    }
+}
